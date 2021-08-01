860d5d3019c071eb7eccf74db223d0fb4842fe54
Hussam Albadri <hussamalbadri2@gmail.com>
2021-08-01 22:23:24 +0300

    Web Page

---
+++ app.js
@@ -0,0 +1,16 @@
+let UserName = prompt("What is your name...?")
+
+alert("Welcome MR/MRS....." + UserName)
+
+let Q1 = prompt("Enter your Age...")
+document.write('<h2>' + Q1 + '<h2>')
+
+let Q2 = prompt("What do you know about Our website?")
+document.write('<h2>' + Q2 + '<h2>')
+
+let Q3 = prompt("Would you like to take a tour into our website?")
+document.write('<h2>' + Q2 + '<h2>')
+
+let number = 25;
+number = 40;
+console.log(number)
